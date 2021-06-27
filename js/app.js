e2035130f7e7e20e233f127a60622e3c5977befc
Dima Zeklam <dema.ziklam@gmail.com>
2021-06-27 23:28:38 +0300

    class06

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let store1 = {
    minCust: 23 ,
    maxCust: 65 ,
    avgCookiPerCust: 6.3,
-   storeLoc: 'Sattle' ,
+   storeLoc: 'Seattle' ,
    dailyTotle : 0 ,
    hourlySales:[],
    hour: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'] ,

f77fc8a5ba2917f70e40f2da50be0be113432bd3
Dima Zeklam <dema.ziklam@gmail.com>
2021-06-30 15:54:24 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -118,11 +118,33 @@ let Dubai  = new cookieStand(11,38,3.7,'Dubai');
 let Paris  = new cookieStand(20,38,2.3,'Paris');
 let Lima  = new cookieStand(2,16,4.6,'Lima' );
 
+let myform = document.getElementById("myform");
+myform.addEventListener('submit',newstore);
+
+function newstore(event){
+    var rowCount = tableEl.rows.length;
+    tableEl.deleteRow(rowCount-1);
+    event.preventDefault();
+    let storeloc = event.target.storeloc.value;
+    let min = event.target.min.value;
+    let max = event.target.max.value;
+    let avg = event.target.avg.value;
+
+    let newLoc = new cookieStand(min,max,avg,storeloc);
+    newLoc.generalCall();
+   
+  
+    total();
+
+}
 
  
 for(let f=0;f<storsPlaces.length;f++){
     storsPlaces[f].generalCall();
     
 }
+
 total();
 
+  
+myform.addEventListener('submit',newstore);
